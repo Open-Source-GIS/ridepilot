ff0105b47bb29b16a1335b56df3b52608f213361
chrispatterson <dimmerswitch@gmail.com>
2011-03-01 15:51:56 -0600

    Initial commit - includes Rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ridepilot::Application.config.secret_token = 'f8a9e05ef360713a57b61cd1cb30f7194fdaf2bd34aa566bb2df0bb4df47bf3d94e53fd3808d314736f38a61d25254aed581b6e05de6dbd4bd8ea3315f5023c6'
