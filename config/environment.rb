04faf4ee2a52737426d2193318923c911bc0bbce
David Turner <novalis@openplans.org>
2011-03-22 18:37:08 -0400

    list and document result codes

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,13 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+TRIP_RESULT_CODES = {
+  "COMP" => "Completed", # the trip was (as far as we know) completed
+  "CANC" => "Cancelled", # the trip was cancelled by the customer
+  "NS" => "No Show", # the customer did not show up for the trip
+  "TD" => "Turndown", # the provider told the customer that it could not provide the trip
+  "UNMET" => "Unmet Need" #a trip that was outside of the service parameters (too early, too late, too far, etc).
+}
+
 # Initialize the rails application
 Ridepilot::Application.initialize!
