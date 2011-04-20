e9a85b24218dfac5543888049aae938bdce2982b
Andrew Cochran <acochran@obiwan.openplans.org>
2011-04-20 19:22:13 +0000

    change method for styling current menu items to work for subpages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,9 +4,10 @@ module ApplicationHelper
     return text.gsub("\n", "<br/>")
   end
 
-  def link_to_with_highlight(name, options = {}, html_options = {}) # same sig as #link_to
-    html_options.merge!({ :class => 'active' }) if current_page?(options)
-    link_to(name, options, html_options)
+  def bodytag_class
+    a = controller.class.to_s.underscore.gsub(/_controller$/, '')
+    b = controller.action_name.underscore
+    "#{a} #{b}".gsub(/_/, '-')
   end
 
 end
