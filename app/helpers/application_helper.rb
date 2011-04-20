e39c7751a5b4e96c3fb3dded1750b79b2a450682
Andrew Cochran <acochran@obiwan.openplans.org>
2011-04-20 17:10:05 +0000

    markup & style changes for navigations and forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,10 @@ module ApplicationHelper
   def format_newlines(text)
     return text.gsub("\n", "<br/>")
   end
+
+  def link_to_with_highlight(name, options = {}, html_options = {}) # same sig as #link_to
+    html_options.merge!({ :class => 'active' }) if current_page?(options)
+    link_to(name, options, html_options)
+  end
+
 end
