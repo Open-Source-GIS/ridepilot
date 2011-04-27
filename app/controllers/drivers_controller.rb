31732d765f06dcee16bba742d9774c24149c6080
David Turner <novalis@openplans.org>
2011-04-27 16:39:52 -0400

    validation and error handling for drivers

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -13,16 +13,22 @@ class DriversController < ApplicationController
 
   def update
     @driver.update_attributes(params[:driver])
-    @driver.save!
-    flash[:notice] = "Driver updated"
-    redirect_to provider_path(current_user.current_provider)
+    if @driver.save
+      flash[:notice] = "Driver updated"
+      redirect_to provider_path(current_user.current_provider)
+    else
+      render :action=>:edit
+    end
   end
 
   def create
     @driver.provider = current_provider
-    @driver.save!
-    flash[:notice] = "Driver created"
-    redirect_to provider_path(current_provider)
+    if @driver.save
+      flash[:notice] = "Driver created"
+      redirect_to provider_path(current_provider)
+    else
+      render :action=>:new
+    end
   end
 
   def destroy
--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,6 +1,6 @@
 class Driver < ActiveRecord::Base
   belongs_to :provider
   validates_uniqueness_of :name, :scope => :provider_id
-
+  validates_length_of :name, :minimum=>2
 
 end
