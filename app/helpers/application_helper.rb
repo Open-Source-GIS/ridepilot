feaba4bb67fafd4d72a67fc8afe08130f032deec
David Turner <novalis@openplans.org>
2011-04-28 16:12:19 -0400

    handle repeating trips in show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,43 @@ module ApplicationHelper
     "#{a} #{b}".gsub(/_/, '-')
   end
 
+  def collect_weekdays(schedule)
+    weekdays = []
+    if schedule.monday
+      weekdays.push :monday
+    end
+    if schedule.tuesday
+      weekdays.push :tuesday
+    end
+    if schedule.wednesday
+      weekdays.push :wednesday
+    end
+    if schedule.thursday
+      weekdays.push :thursday
+    end
+    if schedule.friday
+      weekdays.push :friday
+    end
+    if schedule.saturday
+      weekdays.push :saturday
+    end
+    if schedule.sunday
+      weekdays.push :sunday
+    end
+    return weekdays
+  end
+
+  def weekday_abbrev(weekday)
+    weekday_abbrevs = {
+      :monday => 'M',
+      :tuesday => 'T',
+      :wednesday => 'W',
+      :thursday => 'R',
+      :friday => 'F',
+      :saturday => 'S',
+      :sunday => 'U'
+    }
+
+    return weekday_abbrevs[weekday]
+  end
 end
