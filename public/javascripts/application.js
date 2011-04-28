bc480d42773365b65fa8a450fc9e4de9037f3aa2
Andy Cochran <acochran@openplans.org>
2011-04-28 21:40:41 +0000

    style reports pages

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,5 +2,23 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 $(document).ready(function() {
+
   $("tr:odd").addClass("odd");
+
+  // add time picker functionality
+  // http://trentrichardson.com/examples/timepicker/
+  $('#trip_pickup_time, #trip_appointment_time').datetimepicker({
+  	ampm: false,
+  	hourMin: 8,
+  	hourMax: 18,
+		hourGrid: 3,
+    	minuteGrid: 15,
+		timeFormat: 'hh:mm:ss',
+		dateFormat: 'yy-mm-dd'    		
+  });
+
+  $('#new_monthly #monthly_start_date, #new_monthly #monthly_end_date').datepicker({
+		dateFormat: 'yy-mm-dd'    		
+  });
+
 });
