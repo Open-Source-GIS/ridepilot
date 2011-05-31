79f3d1658e4d2e1277dca007f4a7caab8d212b9b
Julia West <juliamae@gmail.com>
2011-05-31 18:24:48 -0400

    Updating autocomplete-rails.js

--- public/javascripts/autocomplete-rails.js
+++ public/javascripts/autocomplete-rails.js
@@ -6,7 +6,7 @@
 *
 *   Example:
 *       <input type="text" data-autocomplete="/url/to/autocomplete">
-* 
+*
 * Optionally, you can use a jQuery selector to specify a field that can
 * be updated with the element id whenever you find a matching value
 *
@@ -28,27 +28,19 @@ $(document).ready(function(){
 			}
 		});
 	};
-	
+
 	jQuery.railsAutocomplete = function (e) {
 		_e = e;
 		this.init(_e);
 	};
-	
+
 	jQuery.railsAutocomplete.fn = jQuery.railsAutocomplete.prototype = {
 		railsAutocomplete: '0.0.1'
 	};
-	
+
 	jQuery.railsAutocomplete.fn.extend = jQuery.railsAutocomplete.extend = jQuery.extend;
 	jQuery.railsAutocomplete.fn.extend({
 		init: function(e) {
-      e.button_ids = ($(e).attr('submit-buttons') || "").split(" ");
-      for (var i = 0; i < e.button_ids.length; ++i) {
-        var button_id = e.button_ids[i];
-        if ($(button_id)[0]) {
-          $(button_id)[0].disabled = true;
-        }
-      }
-
 			e.delimiter = $(e).attr('data-delimiter') || null;
 			function split( val ) {
 				return val.split( e.delimiter );
@@ -56,12 +48,19 @@ $(document).ready(function(){
 			function extractLast( term ) {
 				return split( term ).pop().replace(/^\s+/,"");
 			}
-		
+
 	    $(e).autocomplete({
 				source: function( request, response ) {
 					$.getJSON( $(e).attr('data-autocomplete'), {
 					term: extractLast( request.term )
-					}, response );
+					}, function() {
+                        $(arguments[0]).each(function(i, el) {
+                            var obj = {};
+                            obj[el.id] = el;
+                            $(e).data(obj);
+                        });
+                        response.apply(null, arguments);
+                    });
 				},
 				search: function() {
 					// custom minLength
@@ -89,17 +88,16 @@ $(document).ready(function(){
 						if ($(this).attr('id_element')) {
 							$($(this).attr('id_element')).val(ui.item.id);
 						}
-						if ($(this).attr('select_func')) {
-                eval($(this).attr('select_func')) (ui.item);
-            }
-					};
+                        if ($(this).attr('data-update-elements')) {
+                            var data = $(this).data(ui.item.id.toString());
+                            var update_elements = $.parseJSON($(this).attr("data-update-elements"));
+                            for (var key in update_elements) {
+                                $(update_elements[key]).val(data[key]);
+                            }
+                        }
+					}
+					$(this).trigger('railsAutocomplete.select');
 
-          for (var i = 0; i < e.button_ids.length; ++i) {
-            var button_id = e.button_ids[i];
-            if ($(button_id)[0]) {
-              $(button_id)[0].disabled = false;
-            }
-          }
 					return false;
 				}
 			});
