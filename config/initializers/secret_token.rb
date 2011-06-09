7df8fc2972e16505ee2de3bb2206a2877fb83927
Kevin Chambers <kc@kevinchambers.com>
2011-06-09 09:51:49 -0700

    change token due to devise upgrade

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Ridepilot::Application.config.secret_token = 'f8a9e05ef360713a57b61cd1cb30f7194fdaf2bd34aa566bb2df0bb4df47bf3d94e53fd3808d314736f38a61d25254aed581b6e05de6dbd4bd8ea3315f5023c6'
+Ridepilot::Application.config.secret_token = 'f8a9e05ef361713a57b61cd1cb30f7194fdaf2bd34aa566bb2df0bb4df47bf3d94e53fd3808d314736f38a61d25254aed581b6e05de6dbd4bd8ea3315f5023c6'
