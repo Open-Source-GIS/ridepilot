6625d81c43707c97a7d1156c4e73ebe1925e87ae
Julia West <juliamae@gmail.com>
2011-08-15 17:16:14 -0400

    Fixing db schema number.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110805222642) do
+ActiveRecord::Schema.define(:version => 20110812184549) do
 
   create_table "addresses", :force => true do |t|
     t.string   "name"
