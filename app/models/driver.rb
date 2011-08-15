e7800340366c6f8caf15b8033f9387e4db9a1072
Julia West <juliamae@gmail.com>
2011-08-15 18:26:21 -0400

    Limiting driver options on vehicle form to those for the current provider.

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -8,4 +8,5 @@ class Driver < ActiveRecord::Base
   stampable :creator_attribute => :created_by_id, :updater_attribute => :updated_by_id
   
   scope :active, where(:active => true)
+  scope :for_provider, lambda { |provider_id| where(:provider_id => provider_id) }
 end
