c92f36871e5aea1f1a33741c478dcab6ade0c3aa
Julia West <juliamae@gmail.com>
2011-09-29 09:50:09 -0500

    Explicitly loading jstree theme stylesheet.

--- public/javascripts/jquery.jstree.js
+++ public/javascripts/jquery.jstree.js
@@ -1442,7 +1442,7 @@
 				if(!theme_name) { return false; }
 				if(!theme_url) { theme_url = $.jstree._themes + theme_name + '/style.css'; }
 				if($.inArray(theme_url, themes_loaded) == -1) {
-					$.vakata.css.add_sheet({ "url" : theme_url });
+          // $.vakata.css.add_sheet({ "url" : theme_url });
 					themes_loaded.push(theme_url);
 				}
 				if(this.data.themes.theme != theme_name) {
