15c0ab9b8532ce500936ab7e1ce11e6841aadb5b
Kevin Chambers <KChambers@RideConnection.org>
2012-03-19 20:27:05 -0700

    That wasn't a typo after all

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
-set :deploy_to, "/home/deployer/rails/ridepilot_staging"
+set :deploy_to, "/home/deployer/rails/ridepilot-staging"
 set :branch, "master"
 set :rvm_ruby_string, '1.9.2'
 role :web, "184.154.79.122"
