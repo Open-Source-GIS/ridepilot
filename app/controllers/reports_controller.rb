3ccd66a7e12c66e6b74639d199ef60312f95c207
Kevin Chambers <kc@kevinchambers.com>
2012-03-28 17:49:28 -0700

    Style the print version of service summary report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -102,6 +102,7 @@ class ReportsController < ApplicationController
     end
     @monthly = Monthly.where(:start_date => @start_date, :provider_id=>current_provider_id).first
     @monthly = Monthly.new(:start_date=>@start_date, :provider_id=>current_provider_id) if @monthly.nil?
+    @provider = current_provider
 
     if !can? :read, @monthly
       return redirect_to reports_path
