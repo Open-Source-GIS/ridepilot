06e41387e6cd0ee12fe7e78e2004acf0e2e4f3c7
Kevin Chambers <kc@kevinchambers.com>
2012-05-31 17:06:27 -0700

    Missed a line

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -56,7 +56,7 @@ class RunsController < ApplicationController
 
     respond_to do |format|
       if @run.save
-        format.html { redirect_to(runs_path, :notice => 'Run was successfully created.') }
+        format.html { redirect_to(runs_path(date_range(@run)), :notice => 'Run was successfully created.') }
         format.xml  { render :xml => @run, :status => :created, :location => @run }
       else
         @drivers = Driver.where(:provider_id=>@run.provider_id)
